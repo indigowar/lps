09f5db72d81140e4498ba73fb68ab5cc48a0ed02
Max Kamerov <indigowar.github@gmail.com>
2023-12-25 18:04:19 +0300

    done

--- internal/app/app.go
+++ internal/app/app.go
@@ -23,7 +23,7 @@ import (
 func Run(cfg *config.Config) {
 	// postgres, err := postgres.CreateConnection(cfg.Db.Host, cfg.Db.Port, cfg.Db.Db, cfg.Db.SystemUser, cfg.Db.SystemPassword)
 
-	postgres, err := postgres.CreateConnectionUsingURL(os.Getenv("URL"))
+	postgres, err := postgres.CreateConnectionUsingURL(os.Getenv("POSTGRES_URL"))
 	if err != nil {
 		log.Fatal(err)
 	}
